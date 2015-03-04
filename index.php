b6984220f85a5eae863054874434e9a65b924045
jimmle <meh38p@gmail.com>
2015-03-04 14:29:22 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+<style>
+p.class{
+font-family:"Verdana";
+font-size:"16";
+}
+ul#menu{
+padding:0
+}
+ul#menu li{
+display:inline;
+}
+ul#menu li a{
+padding:10px 20px;
+border-radius:10px 10px 10px 10px;
+color:white;
+background-color:black;
+text-decoration:none;
+}
+ul#menu li a:hover{
+background-color:grey;
+}
+.button{
+padding:10px 20px;
+border-radius:10px 10px 10px 10px;
+color:white;
+background-color:black;
+text-decoration:none;
+}
+</style>
+</head>
+
+<body>
+<h1>Enjoy these images of fruits!</h1>
+<ul id="menu">
+<li><a href="apples.html">Apples</a></li>
+<li><a href="bananas.html">Bananas</a></li>
+<li><a href="lemons.html">Lemons</a></li>
+<li><a href="oranges.html">Oranges</a></li>
+<li><a href="huehue.html">More fruits</a></li>
+</ul>
+
+<h3>Want even more fruits?</h3>
+
+<div style="margin-left:60px">
+<form action="http://xvideos.com/c/Gay-45">
+<input class="button" type="submit" value="Yes" size="100">
+</form>
+</div>
+
+<p class="class" id="closer">If you like what you see, turn off your PC and do it!</p>
+
+<div style="margin-left:0px">
+<input class="button" style="width:90px;text-align:center;margin-left:30px" onclick="changeCloser()" value="Activate JS">
+</div>
+
+<script>
+function changeCloser(){
+	document.getElementById("closer").innerHTML="Changed!";
+}
+</script>
+</body>
+
+</html>
